1ff057f75c21260f2290520b84f7dd2841ef0eec
Ivan <i85chiosa@gmail.com>
2023-11-29 19:39:01 +0100

    Initial commit

---
+++ src/main/java/com/example/backendprojekt/BackendProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProjektApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/backendprojekt/ToDoService.java
@@ -0,0 +1,2 @@
+package com.example.backendprojekt;public class ToDoService {
+}
